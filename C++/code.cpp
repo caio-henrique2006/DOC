c531db00f14b1df2df8166bcdc137e17ced15e6c
Caio Henrique <Caio_Henrique2006@outlook.com>
2023-12-18 10:49:14 -0300

    .

--- C++/code.cpp
+++ C++/code.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+unsigned short int valor;
+
 int main() 
 {
-    cout << "Hello" << endl;
+    cout << "Digite um valor inteiro: " << endl;
+    cin >> valor;
+
+    valor++;
+
+    cout << "O sucessor do valor inteiro digitado é " << valor << endl;
+
     return 0;
 }
---
+++ C++/exercicios/soma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    float value1, value2, resultado;
+
+    cout << "Digite dois números inteiros:" << endl;
+    cin >> value1 >> value2;
+
+    resultado = value1 + value2;
+
+    cout << "O resultado é " << resultado << endl;
+
+    return 0;
+}
+
