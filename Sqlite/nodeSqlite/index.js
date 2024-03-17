95a5bb712b05ac0f41c24d85da285d952a14d9e9
Caio Henrique <Caio_Henrique2006@outlook.com>
2024-03-17 19:30:26 -0300

    .

--- Sqlite/nodeSqlite/index.js
+++ Sqlite/nodeSqlite/index.js
@@ -1,51 +1,39 @@
-const sqlite3 = require('sqlite3').verbose();
+async function getData (callback) {
+    const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./data/chinook.db', sqlite3.OPEN_READONLY, (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
+    let db = new sqlite3.Database('./data/chinook.db', sqlite3.OPEN_READONLY, (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Connected to the sqlite data");
+    });
 
-    console.log("Connected to the sqlite data");
-});
+    let sql = `
+    SELECT 
+        title
+    FROM
+        albums
+    `;
 
-// Usando db.all:
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        callback(rows);
+    });
 
-// let sql = `
-// SELECT DISTINCT name FROM playlists 
-// ORDER BY name
-// `
-
-// db.all(sql, [], (err, rows) => {
-//     if (err) {
-//         throw err;
-//     }
-//     rows.forEach((row) => {
-//         console.log(row);
-//     })
-// })
-
-// Usando db.get()
-
-let sql = `
-SELECT PlaylistId id, Name name FROM playlists
-WHERE PlaylistId = ?
-`
-
-let playlistId = 1;
-
-db.get(sql, [playlistId], (err, row) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    return row
-    ? console.log(row.id, row.name)
-    : console.log(`No playlist found with the id ${playlistId}`);
-})
-
-
-db.close((err) => {
+    db.close((err) => {
     if (err) {
         return console.error(err.message);
     }
     console.log("Close database connection");
 });
+}
+
+export default function showData () {
+    getData(log);
+}
+
+function log () {
+    console.log(data)
+}
